e53aaa127937221da4393485be77908c4bd3bfa3
Tam Nguyen <tnguyen@smith.edu>
2016-02-03 17:58:24 -0500

    Created part 1 of movie viewer app

---
+++ MovieViewer/MovieCell.swift
@@ -0,0 +1,26 @@
+//
+//  MovieCell.swift
+//  MovieViewer
+//
+//  Created by Tam Nguyen on 2/3/16.
+//  Copyright © 2016 Tam Nguyen. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+    @IBOutlet weak var titleLabel: UILabel!
+
+    @IBOutlet weak var overviewLabel: UILabel!
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
